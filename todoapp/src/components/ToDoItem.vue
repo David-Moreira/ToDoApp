5fed84340b22f7c4235e104ffef0c6929fe95367
David-Moreira <david_nogueira_moreira@hotmail.com>
2019-11-19 01:10:58 +0000

    Tasks + Add New Task + Layout

--- todoapp/src/components/ToDoItem.vue
+++ todoapp/src/components/ToDoItem.vue
@@ -1,14 +1,22 @@
 <template>
-        <div class="todo-item">
-                <strong>{{todo.title}}</strong>
-        </div>
+<v-card class="ma-2" shaped v-bind:color="todo.completed ? 'light-green' : 'dark lighten2' " >
+        <v-card-title class="pa-2"  >
+               <span v-bind:style="todo.completed ? 'textDecoration:line-through;' : 'textDecoration:none;'"> <strong>{{todo.title}}</strong></span>
+                        <v-spacer/>
+                        
+        <v-btn icon shaped @click="$emit('sel-todo', todo.id)" v-bind:color="todo.completed ? 'light-green darken-4' : 'dark lighten2' " >
+                <v-icon>mdi-checkbox-marked-circle</v-icon>
+        </v-btn>
+        </v-card-title>
+
+</v-card>
 </template>
 
 <script>
 export default {
     name: "ToDoItem",
     props: {
-        todo: String
+        todo: Object
         }
 }
 </script>
