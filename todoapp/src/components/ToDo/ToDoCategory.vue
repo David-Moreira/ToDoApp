02b83e6ad94af252b49c9750e62b53f9e5de269f
User <User@HEPC1CW6LK>
2019-11-23 00:18:21 +0000

    Loads of changes!!!

---
+++ todoapp/src/components/ToDo/ToDoCategory.vue
@@ -0,0 +1,29 @@
+<template>
+<v-select
+            v-model="select"
+            :items="categories"
+            label="Category:"
+            @change="$emit('category-change',select)"
+          >
+          </v-select>
+</template>
+
+<script>
+export default {
+    name: "ToDo-Category",
+    data(){
+
+        return {
+            select: 0
+        }
+    }
+   , props: 
+    {
+        categories: Array
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
